d1b229bd7b89f9db279bdd3219e26432ff2d6d25
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2023-02-09 17:02:24 +0530

    content change after animation added

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,47 @@ import Navbar from "./components/Navbar";
 import Human from "./components/Human";
 import Collection from "./components/Collection";
 import Angel from "./components/Angel";
-
-
-
+import { useEffect, useRef, useState } from "react";
 
 export default function App() {
+  const UIdata = [
+    {
+    name: "Angel",
+    blade: "/images/angel/blade.png",
+    icon: "/images/angel/icon.png",
+    clone: "/images/angel/clone.png",
+  },
+    {
+      name: "Human",
+      blade: "/images/alien/blade.png",
+      icon: "/images/alien/icon.png",
+      clone: "/images/alien/clone.png",
+    },
+    {
+      name: "genesis",
+      blade: "/images/genesis/blade.png",
+      icon: "/images/genesis/icon.png",
+      clone: "/images/genesis/clone.png",
+    },
+  ];
+  const [increment, setIncrement] = useState(0);
+
+  const [data, setData] = useState({
+    name: "Angel",
+    blade: "/images/angel/blade.png",
+    icon: "/images/angel/icon.png",
+    clone: "/images/angel/clone.png",
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIncrement((i) => (i === 2 ? 0 : i + 1));
+      setData(UIdata[increment]);
+    }, 8000);
+    return () => clearInterval(intervalId);
+  }, [increment]);
+  console.log(increment, data);
+
   return (
     <div>
       <div className="space stars1"></div>
@@ -17,7 +53,7 @@ export default function App() {
       <Navbar />
 
       {/* <Genesis/> */}
-      <Human/>
+      <Human data={data} />
       {/* <Angel/>  */}
       {/* <Collection/> */}
     </div>
--- src/components/Human.jsx
+++ src/components/Human.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState , useRef} from "react";
 import Collection from "./Collection";
 
-const Human = () => {
+const Human = ({data}) => {
+const {name,icon,blade,clone} = data
   const [translateX, setTranslateX] = useState(0);
   const [translateY, setTranslateY] = useState(0);
 
@@ -30,12 +31,27 @@ const Human = () => {
     if (isAnimating) {
       const timer = setTimeout(() => {
         setIsAnimating(false);
-      }, 5000);
+      }, 3000);
       return () => clearTimeout(timer);
     }
   }, [isAnimating]);
 
-  console.log(isAnimating);
+  // const [scrollState, setScrollState] = useState(true);
+  // const containerRef = useRef(null);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+  //     if (scrollTop === 0) {
+  //       setScrollState(false);
+  //     }
+  //     if (scrollTop + clientHeight === scrollHeight) {
+  //       setScrollState(false);
+  //     }
+  //   };
+  //   containerRef.current.addEventListener("scroll", handleScroll);
+  // }, [scrollState]);
+ 
 
   return (
     <div className="alien_back mx-40 px-20  my-4  text-white relative uppercase ">
@@ -59,7 +75,7 @@ const Human = () => {
                 transform: `translate(-${translateX}px, ${translateY}px)`,
               }}
             >
-              Human
+              {name}
             </p>
             <p
               className="trans2 human_font  leading-none -mt-20"
@@ -67,7 +83,7 @@ const Human = () => {
                 transform: `translate(-${translateX}px, ${translateY}px)`,
               }}
             >
-              Human
+              {name}
             </p>
             <p
               className="human_text z-0 leading-none -mt-16 "
@@ -75,7 +91,7 @@ const Human = () => {
                 transform: `translate(-${translateX}px, ${translateY}px)`,
               }}
             >
-              Human
+              {name}
             </p>
           </div>
         </div>
@@ -84,21 +100,21 @@ const Human = () => {
             isAnimating ? "opacity-0" : "opacity-100"
           }`}
         >
-          <img src="/images/alien/blade.png" alt="" />
+          <img src={blade} alt="" />
         </div>
         <div
           className={`spinicon3D w-60 absolute left-[700px] top-10  z-20 ${
             isAnimating ? "opacity-0" : "opacity-100"
           } `}
         >
-          <img src="/images/alien/icon.png" alt="" />
+          <img src={icon} alt="" />
         </div>
         <div
           className={`w-[600px] ${
             isAnimating ? "alien-diappear" : "alien"
           }   z-20`}
         >
-          <img src="/images/alien/clone.png" alt="" />
+          <img src={clone} alt="" />
         </div>
       </div>
       <div
